2acb36d74f1890d9bd5e0913e719d4728a0ac635
Bendyworks Pair <dev@bendyworks.com>
2011-12-21 11:58:57 -0600

    SPIKE: get libPusher logging

--- TravisCI/BWAppDelegate.h
+++ TravisCI/BWAppDelegate.h
@@ -7,14 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "PTPusherDelegate.h"
+#import "PTPusher.h"
 
-@interface BWAppDelegate : UIResponder <UIApplicationDelegate>
+@interface BWAppDelegate : UIResponder <UIApplicationDelegate, PTPusherDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
 @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (strong) PTPusher *pusher;
 
 - (void)saveContext;
 - (NSURL *)applicationDocumentsDirectory;
