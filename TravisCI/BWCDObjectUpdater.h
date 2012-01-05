8d21d5c0aa1dabd93c8f50e0f06a9ada0beed93d
Bendyworks Pair <dev@bendyworks.com>
2012-01-05 16:21:37 -0600

    Add BWCDObjectUpdater ORM layer/singleton

---
+++ TravisCI/BWCDObjectUpdater.h
@@ -0,0 +1,15 @@
+//
+//  BWCDObjectUpdater.h
+//  TravisCI
+//
+//  Created by Bradley Grzesiak on 1/5/12.
+//  Copyright (c) 2012 Bendyworks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BWCDObjectUpdater : NSObject
+
++ (void)updateRepositoryFromDictionary:(NSDictionary *)repositoryDictionary;
+
+@end
