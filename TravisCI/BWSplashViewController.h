45dc4fc2a3d1257aecf3c9a3404708d8f7c8f20e
Bendyworks Pair <dev@bendyworks.com>
2012-01-10 14:39:28 -0600

    WIP add splash view controller

---
+++ TravisCI/BWSplashViewController.h
@@ -0,0 +1,13 @@
+//
+//  BWSplashViewController.h
+//  TravisCI
+//
+//  Created by Bradley Grzesiak on 1/10/12.
+//  Copyright (c) 2012 Bendyworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWSplashViewController : UIViewController <UISplitViewControllerDelegate>
+
+@end
---
+++ TravisCI/BWSplashViewController.m
@@ -0,0 +1,90 @@
+//
+//  BWSplashViewController.m
+//  TravisCI
+//
+//  Created by Bradley Grzesiak on 1/10/12.
+//  Copyright (c) 2012 Bendyworks. All rights reserved.
+//
+
+#import "BWSplashViewController.h"
+
+@interface BWSplashViewController()
+
+@property (strong, nonatomic) UIPopoverController *masterPopoverController;
+
+@end
+
+
+
+@implementation BWSplashViewController
+
+@synthesize masterPopoverController = _masterPopoverController;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+#pragma mark - Split view
+
+- (void)splitViewController:(UISplitViewController *)splitController willHideViewController:(UIViewController *)viewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)popoverController
+{
+    barButtonItem.title = NSLocalizedString(@"Repositories", @"Repositories");
+    [self.navigationItem setLeftBarButtonItem:barButtonItem animated:YES];
+    self.masterPopoverController = popoverController;
+}
+
+- (void)splitViewController:(UISplitViewController *)splitController willShowViewController:(UIViewController *)viewController invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem
+{
+    // Called when the view is shown again in the split view, invalidating the button and popover controller.
+    [self.navigationItem setLeftBarButtonItem:nil animated:YES];
+    self.masterPopoverController = nil;
+}
+
+
+@end
