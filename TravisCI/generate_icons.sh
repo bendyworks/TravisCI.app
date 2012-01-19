ff770fbd77e1fdfccf3fcfb10e78103ef09c381c
Bendyworks Pair <dev@bendyworks.com>
2012-01-19 16:16:40 -0600

    Update image sizes

--- TravisCI/generate_icons.sh
+++ TravisCI/generate_icons.sh
@@ -9,11 +9,11 @@
 set -x
 set -e
 
-convert icon.1000.png -geometry 29@29 Icon-Small.png
-convert icon.1000.png -geometry 50@50 Icon-Small-50.png
-convert icon.1000.png -geometry 57@57 Icon.png
-convert icon.1000.png -geometry 58@58 Icon-Small@2x.png
-convert icon.1000.png -geometry 72@72 Icon-72.png
-convert icon.1000.png -geometry 114@114 Icon@2x.png
-convert icon.1000.png -geometry 512@512 "iTunesArtwork (512x512px)"
+convert icon.1000.png -geometry 29x29 Icon-Small.png
+convert icon.1000.png -geometry 50x50 Icon-Small-50.png
+convert icon.1000.png -geometry 57x57 Icon.png
+convert icon.1000.png -geometry 58x58 Icon-Small@2x.png
+convert icon.1000.png -geometry 72x72 Icon-72.png
+convert icon.1000.png -geometry 114x114 Icon@2x.png
+convert icon.1000.png -geometry 512x512 "iTunesArtwork (512x512px)"
 cp "iTunesArtwork (512x512px)" iTunesArtwork
